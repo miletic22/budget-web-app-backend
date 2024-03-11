1e2271fde48f8bb2674454b8e81c98429dadc7c6
miletic22 <107651545+miletic22@users.noreply.github.com>
2024-03-11 16:14:07 -0700

    Update config.py

--- server/app/config.py
+++ server/app/config.py
@@ -16,12 +16,12 @@ class Settings(BaseSettings):
 
 
 settings = Settings(
-    database_hostname = "aws-0-us-west-1.pooler.supabase.com"
-    database_port = "5432"
-    database_password = "NHcvQRn9(G%urTB"
-    database_name = "Budgeter"
-    database_username = "postgres"
-    secret_key = "427868.3b1Z3_266J@EwtyW*&*5b@3c9R4b.73f42_f821W5ccfe1470c5a.6d100e1ceWMzx51ed1!7b383ad5sq23XzCe5"
-    algorithm = "HS256"
-    access_token_expire_minutes = "30"
+    database_hostname = "aws-0-us-west-1.pooler.supabase.com",
+    database_port = "5432",
+    database_password = "NHcvQRn9(G%urTB",
+    database_name = "Budgeter",
+    database_username = "postgres",
+    secret_key = "427868.3b1Z3_266J@EwtyW*&*5b@3c9R4b.73f42_f821W5ccfe1470c5a.6d100e1ceWMzx51ed1!7b383ad5sq23XzCe5",
+    algorithm = "HS256",
+    access_token_expire_minutes = "30",
 )
